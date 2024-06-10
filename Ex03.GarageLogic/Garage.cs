4437346d114a3db4ab9d68a9e726fe63e3b42cec
Lahav Rabinovitz <lahav.surf@gmail.com>
2024-06-10 22:05:50 +0300

    Started to build the Garage and Vehicle

---
+++ Ex03.GarageLogic/Garage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Vehicle;
+
+namespace Garage
+{
+    public enum eVehicleStatus
+    {
+        InRepair,
+        WasRepair,
+        WasPaidFor
+    }
+
+    internal class Garage
+    {
+        internal class VehicleDetails
+        {
+            string m_OwnerName;
+            string m_OwnerPhoneNumber;
+            eVehicleStatus m_VehicleStatus;
+            Vehicle m_Vehicle;
+        }
+
+        public void ReEnterVehicleToGarage(string i_LicensePlateID);
+        public bool IsVehicleInSystem()
+        {
+
+        }
+        public abstract void EnterNewVehicleToGarage();
+        public List<Vehicle> getVehiclesLicensePlateList(eVehicleStatus i_VehicleStatusWanted)
+        {
+
+        }
+        public void ChangeVehicleStatus(string i_LicensePlateID, eVehicleStatus i_VehicleStatusToChange)
+        {
+
+        }
+        public void InflateWheelsToMaximum(string i_LicensePlateID)
+        {
+
+        }
+        public void RefuelVehicle(string i_LicensePlateID, eGasTypes i_GasType, float i_AmountToRefuel)
+        {
+
+        }
+        public void ChargeVehicle(string i_LicensePlateID, int i_MinutesToCharge)
+        {
+
+        }
+        public void GetVehicleDetails(string i_LicensePlateID)
+        {
+
+        }
+    }
+}
