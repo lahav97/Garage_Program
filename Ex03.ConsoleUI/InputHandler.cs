72630d4e0f9573c8cd9a1446a0fa5b5a7b69456f
smadar <smadarshoshan@gmail.com>
2024-06-10 22:12:18 +0300

    add basic function decleration

---
+++ Ex03.ConsoleUI/InputHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    internal class InputHandler
+    {
+        public static int GetUserChoice()
+        {
+            string userChoiceString;
+            int userChoiceInt;
+            userChoiceString = Console.ReadLine();
+            int.TryParse(userChoiceString, out userChoiceInt);
+
+            return userChoiceInt;
+        }
+
+        public static string GetLeicensePlate()
+        {
+            Console.WriteLine("Please Enter Vihacle leicense plate:");
+            string leicensePlate = Console.ReadLine();
+
+            return leicensePlate;
+        }
+    }
+}
--- Ex03.ConsoleUI/Program.cs
+++ Ex03.ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,113 @@ namespace Ex03.ConsoleUI
 {
     internal class Program
     {
+
+
+        public void StartGarageProgram()
+        {
+            Garage garage; 
+            int userChoice;
+
+            while (true)
+            {
+                userChoice = printMenuAndGetUserChoice();
+                playChoice(userChoice);
+            }
+        }
+
+        private void playChoice(int i_userChoice)
+        {
+            if (i_userChoice == 1)
+            {
+                EnterVihacleToGarage();
+            }
+            else if (i_userChoice == 2)
+            {
+                showVehicels();
+            }
+            else if (i_userChoice == 3)
+            {
+                changeVehicleSitoation();
+            }
+            else if (i_userChoice == 4)
+            {
+                inflateVehicleWheels();
+            }
+            else if (i_userChoice == 5)
+            {
+                feuelGasVehicle();
+            }
+            else if (i_userChoice == 6)
+            {
+                chargeElectricVehicle();
+            }
+            else if (i_userChoice == 7)
+            {
+                showAllInformationForAVehicle();
+            }
+        }
+
+        private int printMenuAndGetUserChoice()
+        {
+            Console.WriteLine(@"
+Please choose what you want to do:
+
+1. Enter vehicle into Garage.
+2. Show all vehicales in Garage.
+3. Change vehicle Situation.
+4. Inflate vehicle wheels.
+5. Feuel gas vehicle.
+6. Charge Electric vehicle.
+7. Show all information for a vehicle.
+
+please write choice number: ");
+
+            return InputHandler.GetUserChoice();
+        }
+
+        private void EnterVihacleToGarage()
+        {
+            string leicensePlate = InputHandler.GetLeicensePlate();
+            if (garage.IsVehicleInSystem(leicensePlate))
+            {
+                garage.ReEnterVehicleToGarage(leicensePlate);
+            }
+            else
+            {
+
+            }
+        }
+
+
+
+        private void showVehicels()
+        {
+
+        }
+
+        private void changeVehicleSitoation()
+        {
+
+        }
+
+        private void inflateVehicleWheels()
+        {
+
+        }
+
+        private void feuelGasVehicle()
+        {
+
+        }
+
+        private void chargeElectricVehicle()
+        {
+
+        }
+
+        private void showAllInformationForAVehicle()
+        {
+
+        }
     }
 }
