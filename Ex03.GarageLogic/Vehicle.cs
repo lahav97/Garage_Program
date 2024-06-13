4da843fa8c7d446612fc881110aa936c0dab37ae
Lahav Rabinovitz <lahav.surf@gmail.com>
2024-06-13 21:07:52 +0300

    Revert "testy"
    This reverts commit df0ee0e145f4583791a1465faefb997fe4758caa.

--- Ex03.GarageLogic/Vehicle.cs
+++ Ex03.GarageLogic/Vehicle.cs
@@ -171,7 +171,6 @@ namespace Vehicles
         internal float m_MinAirPressure = 0;
         internal string m_ManufactureName;
         internal float m_AirPressure;
-        
 
         internal float MaxAirPressure
         {  
