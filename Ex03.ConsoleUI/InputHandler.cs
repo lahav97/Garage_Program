bb054e046c4ffa917e9447d6f80e6a9274e167db
Lahav Rabinovitz <130746203+lahav97@users.noreply.github.com>
2024-06-16 18:06:51 +0300

    fix the merge

--- Ex03.ConsoleUI/InputHandler.cs
+++ Ex03.ConsoleUI/InputHandler.cs
@@ -1,25 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
+using static GarageLogic.Vehicles.Types.Car.CarInfor;
+using static GarageLogic.Vehicles.Types.Motorcycle.MotorcycleInfo;
 
 namespace Ex03.ConsoleUI
 {
     internal class InputHandler
     {
-        public static int GetUserChoice()
-        {
-            string userChoiceString;
-            int userChoiceInt;
-            userChoiceString = Console.ReadLine();
-            int.TryParse(userChoiceString, out userChoiceInt);
-
-            return userChoiceInt;
-        }
-
-        public static string GetLeicensePlate()
+        internal static string GetLicensePlate()
         {
             Console.WriteLine("Please Enter Vihacle leicense plate:");
             string leicensePlate = Console.ReadLine();
@@ -27,9 +14,131 @@ namespace Ex03.ConsoleUI
             return leicensePlate;
         }
 
-        /*public void bool CheckIfInputIsCorrect(string i_input)
+        internal static bool GetYesOrNoAnswer()
         {
-            if (string.IsNullOrWhiteSpace(i_input))
-        }*/
+            while (true)
+            {
+                Console.WriteLine("Please answer y/n:");
+                string answer = Console.ReadLine();
+                if (answer == "y" || answer == "n")
+                {
+                    return answer == "y";
+                }
+
+                Console.WriteLine("You entered the wrong input.");
+            }
+        }
+
+        internal static string GetAStringFromUser(string i_wantedFormat)
+        {
+            string inputName;
+            bool firstTimeInLoop = true;
+
+            do
+            {
+                if (!firstTimeInLoop)
+                {
+                    Console.WriteLine($"The {i_wantedFormat} You enterd is incorect, please enter again:");
+                }
+
+                inputName = Console.ReadLine();
+                firstTimeInLoop = false;
+            } while (string.IsNullOrEmpty(inputName));
+
+            return inputName;
+        }
+
+        internal static int GetInputNumberFromUser(int i_minmumNumber, int i_maximumNumber)
+        {
+            string inputNumberString;
+            int inputNumberInt;
+            bool firstTimeInLoop = true;
+
+            do
+            {
+                if (!firstTimeInLoop)
+                {
+                    Console.WriteLine($"The input You enterd is out of range, please enter a number between {i_minmumNumber} - {i_maximumNumber}");
+                }
+
+                inputNumberString = Console.ReadLine();
+                firstTimeInLoop = false;
+            } while (!int.TryParse(inputNumberString, out inputNumberInt) && inputNumberInt >= i_minmumNumber && inputNumberInt <= i_maximumNumber);
+
+            return inputNumberInt;
+        }
+
+        internal static float GetFloatFromUser()
+        {
+            float inputNumberfloat;
+            bool firstTimeInLoop = true;
+
+            do
+            {
+                if (!firstTimeInLoop)
+                {
+                    Console.WriteLine($"The input You enterd is incurrect");
+                }
+                firstTimeInLoop = false;
+
+            } while (!float.TryParse(Console.ReadLine(), out inputNumberfloat) && inputNumberfloat > 0);
+
+            return inputNumberfloat;
+        }
+
+        internal static eMotorcycleLicenseType GetMotorcycleLicenseType()
+        {
+            Console.WriteLine("Please enter one of the following options:");
+            foreach (eMotorcycleLicenseType motorcycleLicenseType in Enum.GetValues(typeof(eMotorcycleLicenseType)))
+            {
+                Console.WriteLine($"{(int)motorcycleLicenseType}. {motorcycleLicenseType}");
+            }
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int inputNumber))
+                {
+                    if (Enum.IsDefined(typeof(eMotorcycleLicenseType), inputNumber))
+                    {
+                        return (eMotorcycleLicenseType)inputNumber;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice, please try again");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a numeric value.");
+                }
+            }
+        }
+
+        internal static eCarColors GetCarColors()
+        {
+            Console.WriteLine("Please enter one of the following options:");
+            foreach (eCarColors motorcycleLicenseType in Enum.GetValues(typeof(eCarColors)))
+            {
+                Console.WriteLine($"{(int)motorcycleLicenseType}. {motorcycleLicenseType}");
+            }
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int inputNumber))
+                {
+                    if (Enum.IsDefined(typeof(eCarColors), inputNumber))
+                    {
+                        return (eCarColors)inputNumber;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice, please try again");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a numeric value.");
+                }
+            }
+        }
     }
 }
